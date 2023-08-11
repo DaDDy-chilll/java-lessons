7c291da18dc33359f9a7c4dd8f2903bc46d16088
DaDDy <blacklistuser420@gmail.com>
2023-08-11 17:21:04 +0630

    update

--- Assignment01/src/MidpointFindingKarel.java
+++ Assignment01/src/MidpointFindingKarel.java
@@ -3,9 +3,30 @@ import stanford.karel.Karel;
 public class MidpointFindingKarel extends Karel {
 
     public void run() {
-
+        putBeeper();
+        goForward();
+        putBeeper();
+        turnAround();
+        goToBeeper();
+        turnAround();
     }
 
+    void goToBeeper(){
+        move();
+        while (noBeepersPresent()){
+            move();
+        }
+    }
+    void goForward(){
+        while(frontIsClear()){
+            move();
+        }
+    }
+
+    void turnAround(){
+        turnLeft();
+        turnLeft();
+    }
 
 }
 

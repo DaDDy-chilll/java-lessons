8e53a4bd0e8b041c5a18c4d0f404eac6c472a6d3
DaDDy <blacklistuser420@gmail.com>
2023-08-12 09:44:48 +0630

    midpointcomplete

--- Assignment01/src/MidpointFindingKarel.java
+++ Assignment01/src/MidpointFindingKarel.java
@@ -3,12 +3,56 @@ import stanford.karel.Karel;
 public class MidpointFindingKarel extends Karel {
 
     public void run() {
-        putBeeper();
-        goForward();
-        putBeeper();
+
+           findMid();
+
+
+    }
+
+    void findMid(){
+        putBeeperEndOftheSide();
         turnAround();
         goToBeeper();
+        putBeeperDown();
+        clearRightBeeper();
+        clearLeftBeeper();
+        if(rightIsClear()){
+        cameToCenter();
+        }
+    }
+    void cameToCenter(){
+
+            turnAround();
+
+        goToBeeper();
+        move();
+        print(" program end");
+
+    }
+    void  clearRightBeeper(){
+       while (frontIsClear()){
+           move();
+           if(beepersPresent()) {
+               pickBeeper();
+           }
+       }
+    }
+    void clearLeftBeeper(){
         turnAround();
+        goToBeeper();
+        move();
+        while (frontIsClear()){
+            move();
+            pickBeeper();
+        }
+
+    }
+    void putBeeperDown(){
+        while (noBeepersPresent()){
+            putBeeper();
+            turnAround();
+            goToBeeper();
+        }
     }
 
     void goToBeeper(){
@@ -16,13 +60,22 @@ public class MidpointFindingKarel extends Karel {
         while (noBeepersPresent()){
             move();
         }
+        goBackWard();
+
     }
-    void goForward(){
+    void putBeeperEndOftheSide(){
+        putBeeper();
         while(frontIsClear()){
             move();
         }
+        putBeeper();
     }
 
+    void goBackWard(){
+        turnAround();
+        move();
+        turnAround();
+    }
     void turnAround(){
         turnLeft();
         turnLeft();
